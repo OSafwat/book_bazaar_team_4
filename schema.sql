0babd303b7967ab0c6f734b47671b9b88223515f
OSafwat <omarsafsab2004@gmail.com>
2025-07-24 20:45:18 +0300

    added schema file

---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE book_bazaar;
+USE book_bazaar;
+
+CREATE TABLE users (
+	user_id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(70) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    username VARCHAR(100) NOT NULL,
+    phone_number CHAR(11) NOT NULL,
+    country VARCHAR(30) NOT NULL
+)
