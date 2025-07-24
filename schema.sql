aab8373e9baebb57e9d23dd3aee1b892d206ca22
OSafwat <omarsafsab2004@gmail.com>
2025-07-24 20:48:10 +0300

    added authors table

--- schema.sql
+++ schema.sql
@@ -8,4 +8,10 @@ CREATE TABLE users (
     username VARCHAR(100) NOT NULL,
     phone_number CHAR(11) NOT NULL,
     country VARCHAR(30) NOT NULL
-)
+);
+
+CREATE TABLE authors (
+	author_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL
+);
