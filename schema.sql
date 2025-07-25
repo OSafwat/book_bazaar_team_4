0b692e3625839d350889575d46679f4911df7e28
Mohammed Yasser <126705095+mohammedYasser11@users.noreply.github.com>
2025-07-25 16:16:02 +0300

    split the insertion from schema.sql

--- schema.sql
+++ schema.sql
@@ -31,20 +31,3 @@ CREATE TABLE users (
     password_hash VARCHAR(255) NOT NULL,
     country VARCHAR(30) NOT NULL
 );
-
-INSERT INTO authors(first_name, last_name) VALUES
-('Paulo', 'Coelho'),
-('Robert', 'Kiyosaki'),
-('Charles', 'Dickens');
-
-INSERT INTO books(isbn, title, genre, page_count, cost) VALUES
-('abcde12345fghij', 'The Alchemist', 'Drama', 250, 200),
-('lmnop12345andfb', 'Rich Dad Poor Dad', 'Personal Finance', 200, 150),
-('xyzandabc123456', 'The Spy', 'Spy Fiction', 300, 180),
-('123qrstuvwxyz45', 'Oliver Twist', 'Adventure Fiction', 500, 300),
-('cristianoisgoat', 'David Copperfield', 'Autobiography Fiction', 400, 350);
-
-INSERT INTO users(username, phone_number, email, password_hash, country) VALUES
-('MohamedNabil500', '01011388226', 'mohameddnabill04@gmail.com', 'wxyz1234abcd', 'Egypt'),
-('OmarSafwat500', '01020304050', 'omarsafwat03@gmail.com', 'aa12bb34cc56', 'Egypt'),
-('AbdelkawyNasr123', '01000055566', 'abdonasr103@gmail.com', 'qwerty123456', 'Egypt');
